91fe1148170f79ffb419e22bc02db47c57f9064f
Anton <WinterSilence@users.noreply.github.com>
2014-03-28 06:54:21 -0700

    Update plugin.php
    перевод для плагинов уже доступен

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -6,8 +6,8 @@
  */
 
 return array(
-    'name'        => /*_wp*/('Pickup Points'),
-    'description' => /*_wp*/('Pickup points by regions (customer selects a pickup point from the list).'),
+    'name'        => _wp('Pickup Points'),
+    'description' => _wp('Pickup points by regions (customer selects a pickup point from the list).'),
     'icon'        => 'img/delivery16.png', //path to module 16x16 icon
     'logo'        => 'img/delivery.png', //path to module logo (recommended logo size: 60x32)
     'version'     => '1.2',
